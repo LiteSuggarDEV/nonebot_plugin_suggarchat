349be7b414eef620f1e95e761cf5e20c5eb12d97
RAX4096 <windowserror@163.com>
2026-08-21 22:31:04 +0800

    feat: add startup banner and update amrita dependency
    - Print SuggarChat ASCII banner and startup log in onEnable
    - Bump nonebot-plugin-amrita to >=0.2.2
    - Add setuptools package discovery for nonebot_plugin_suggarchat
    -

--- nonebot_plugin_suggarchat/preprocess.py
+++ nonebot_plugin_suggarchat/preprocess.py
@@ -5,6 +5,15 @@ from .hook_manager import run_hooks
 
 driver = get_driver()
 
+STARTUP_BANNER = r"""
+ ____                                ____ _           _    __     ___  _
+/ ___| _   _  __ _  __ _  __ _ _ __ / ___| |__   __ _| |_  \ \   / / || |
+\___ \| | | |/ _` |/ _` |/ _` | '__| |   | '_ \ / _` | __|  \ \ / /| || |_
+ ___) | |_| | (_| | (_| | (_| | |  | |___| | | | (_| | |_    \ V / |__   _|
+|____/ \__,_|\__, |\__, |\__,_|_|   \____|_| |_|\__,_|\__|    \_/     |_|
+             |___/ |___/
+"""
+
 
 @driver.on_bot_connect
 async def hook():
@@ -15,6 +24,8 @@ async def hook():
 @driver.on_startup
 async def onEnable():
     logger.debug("加载配置文件...")
+    print(STARTUP_BANNER)
+    logger.info("SuggarChat V4 启动中...")
     # safe_get_config 触发 uniconf 的 _init → add_config 流程，
     # 由 on_reload 回调完成 config/ins_config 赋值后再加载。
     await config_manager.safe_get_config()
